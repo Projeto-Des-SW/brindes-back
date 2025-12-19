50b5b2a0916912b85bb4c721125eb60b30ca892a
Carlos Eduardo Bahia <ter_caraujo@arezzo.com.br>
2025-12-19 08:52:45 -0300

    Initial Commit

---
+++ src/main/java/br/ed/ufape/bahiabrindes/BahiabrindesApplication.java
@@ -0,0 +1,13 @@
+package br.ed.ufape.bahiabrindes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahiabrindesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahiabrindesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ed/ufape/bahiabrindes/BahiabrindesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ed.ufape.bahiabrindes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahiabrindesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
